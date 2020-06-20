b57ed58a07eaf9ad25259b92ce6e4677c8a11d53
Kamil <krusiec.kamil82@gmail.com>
2020-06-20 14:21:32 +0200

    Setting Up The Intro Screen And Moving To It After 2 Seconds

--- app/src/main/java/pl/krusiec/trelloclone/SplashActivity.kt
+++ app/src/main/java/pl/krusiec/trelloclone/SplashActivity.kt
@@ -1,8 +1,10 @@
 package pl.krusiec.trelloclone
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -19,5 +21,10 @@ class SplashActivity : AppCompatActivity() {
 
         val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
         tvAppName.typeface = typeFace
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 2500)
     }
 }
