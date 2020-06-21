f199cc4f83b3258a56f2f5ea4c3076f0738d2297
Kamil <krusiec.kamil82@gmail.com>
2020-06-21 15:50:57 +0200

    Preparing The SignUp Feature in Firebase And In The App

--- app/src/main/java/pl/krusiec/trelloclone/activities/BaseActivity.kt
+++ app/src/main/java/pl/krusiec/trelloclone/activities/BaseActivity.kt
@@ -52,7 +52,7 @@ open class BaseActivity : AppCompatActivity() {
         }
     }
 
-    fun showErrorSnackBar(message: String){
+    fun showErrorSnackBar(message: String) {
         val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
         val snackBarView = snackBar.view
         snackBarView.setBackgroundColor(ContextCompat.getColor(this, R.color.snackbar_error_color))
--- app/src/main/java/pl/krusiec/trelloclone/activities/SignUpActivity.kt
+++ app/src/main/java/pl/krusiec/trelloclone/activities/SignUpActivity.kt
@@ -1,12 +1,13 @@
 package pl.krusiec.trelloclone.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import pl.krusiec.trelloclone.R
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +29,38 @@ class SignUpActivity : AppCompatActivity() {
 
         toolbarSignUpActivity.setNavigationOnClickListener {
             onBackPressed()
+        }
+
+        btnSignUp.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val name: String = etName.text.toString().trim { it <= ' ' }
+        val email: String = etEmail.text.toString().trim { it <= ' ' }
+        val password: String = etPassword.text.toString().trim { it <= ' ' }
+
+        if (validateForm(name, email, password)) {
+            Toast.makeText(this, "Now we can register a new user.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter a email address")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> true
         }
     }
 }
