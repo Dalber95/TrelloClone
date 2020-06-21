3110bf9afa04674f47508a23b2c8b88e334c6d28
Kamil <krusiec.kamil82@gmail.com>
2020-06-21 16:20:51 +0200

    Registering The User Via Email in Firebase

--- app/src/main/java/pl/krusiec/trelloclone/activities/SignUpActivity.kt
+++ app/src/main/java/pl/krusiec/trelloclone/activities/SignUpActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.WindowManager
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import pl.krusiec.trelloclone.R
 
@@ -42,7 +44,24 @@ class SignUpActivity : BaseActivity() {
         val password: String = etPassword.text.toString().trim { it <= ' ' }
 
         if (validateForm(name, email, password)) {
-            Toast.makeText(this, "Now we can register a new user.", Toast.LENGTH_SHORT).show()
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        val registeredEmail = firebaseUser.email
+                        Toast.makeText(
+                            this,
+                            "$name you have successfully registered the email address $registeredEmail",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        FirebaseAuth.getInstance().signOut()
+                        finish()
+                    } else {
+                        Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 
