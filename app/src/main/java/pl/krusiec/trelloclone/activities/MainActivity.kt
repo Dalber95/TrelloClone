a5acfbd48eb43b1755ef0db93ecc93bf376009da
Kamil <krusiec.kamil82@gmail.com>
2020-06-22 18:03:55 +0200

    Adding The Drawer Functionality In Code

--- app/src/main/java/pl/krusiec/trelloclone/activities/MainActivity.kt
+++ app/src/main/java/pl/krusiec/trelloclone/activities/MainActivity.kt
@@ -1,12 +1,65 @@
 package pl.krusiec.trelloclone.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 import pl.krusiec.trelloclone.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBar()
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbarMainActivity)
+        toolbarMainActivity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        toolbarMainActivity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navMyProfile -> {
+                Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show()
+            }
+            R.id.navSignOut -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
