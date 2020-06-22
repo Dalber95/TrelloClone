11403b53fde3b1b177f9f77fb4115fecae04f835
Kamil <krusiec.kamil82@gmail.com>
2020-06-22 14:51:57 +0200

    Signing In And Getting The User Data

--- app/src/main/java/pl/krusiec/trelloclone/activities/SignInActivity.kt
+++ app/src/main/java/pl/krusiec/trelloclone/activities/SignInActivity.kt
@@ -3,12 +3,13 @@ package pl.krusiec.trelloclone.activities
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.view.WindowManager
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import pl.krusiec.trelloclone.R
+import pl.krusiec.trelloclone.firebase.FirestoreClass
+import pl.krusiec.trelloclone.models.User
 
 class SignInActivity : BaseActivity() {
 
@@ -29,6 +30,12 @@ class SignInActivity : BaseActivity() {
         setupActionBar()
     }
 
+    fun signInSuccess(user: User) {
+        hideProgressDialog()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
     private fun setupActionBar() {
         setSupportActionBar(toolbarSignInActivity)
 
@@ -46,14 +53,10 @@ class SignInActivity : BaseActivity() {
         if (validateForm(email, password)) {
             showProgressDialog(resources.getString(R.string.please_wait))
             auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
-                hideProgressDialog()
                 if (task.isSuccessful) {
-                    Log.d("Sign in", "signInWithEmail:success")
-                    val user = auth.currentUser
-                    startActivity(Intent(this, MainActivity::class.java))
+                    FirestoreClass().signInUser(this)
                 } else {
-                    Log.w("Sign in", "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_LONG).show()
                 }
             }
         }
--- app/src/main/java/pl/krusiec/trelloclone/firebase/FirestoreClass.kt
+++ app/src/main/java/pl/krusiec/trelloclone/firebase/FirestoreClass.kt
@@ -3,6 +3,7 @@ package pl.krusiec.trelloclone.firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
+import pl.krusiec.trelloclone.activities.SignInActivity
 
 import pl.krusiec.trelloclone.activities.SignUpActivity
 import pl.krusiec.trelloclone.models.User
@@ -21,7 +22,18 @@ class FirestoreClass {
             }
     }
 
-    fun getCurrentUserId(): String {
+    fun signInUser(activity: SignInActivity) {
+        fireStore.collection(Constants.USERS)
+            .document(getCurrentUserId())
+            .get()
+            .addOnSuccessListener { document ->
+                val loggedInUser = document.toObject(User::class.java)
+                if (loggedInUser != null)
+                    activity.signInSuccess(loggedInUser)
+            }
+    }
+
+    private fun getCurrentUserId(): String {
         return FirebaseAuth.getInstance().currentUser!!.uid
     }
 }
