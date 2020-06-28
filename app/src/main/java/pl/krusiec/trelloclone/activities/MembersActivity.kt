ce5646702057b4dd111f56845f7bf8c99f08c881
Kamil <krusiec.kamil82@gmail.com>
2020-06-28 16:06:39 +0200

    Adding New Members - UI

--- app/src/main/java/pl/krusiec/trelloclone/activities/MembersActivity.kt
+++ app/src/main/java/pl/krusiec/trelloclone/activities/MembersActivity.kt
@@ -1,9 +1,13 @@
 package pl.krusiec.trelloclone.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_members.*
+import kotlinx.android.synthetic.main.dialog_search_member.*
 import pl.krusiec.trelloclone.R
 import pl.krusiec.trelloclone.adapters.MemberListItemsAdapter
 import pl.krusiec.trelloclone.firebase.FirestoreClass
@@ -46,5 +50,39 @@ class MembersActivity : BaseActivity() {
         actionBar?.title = resources.getString(R.string.members)
 
         toolbarMembersActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add_member, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_add_member ->{
+                dialogSearchMember()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun dialogSearchMember(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_search_member)
+        dialog.tvAdd.setOnClickListener {
+            val email = dialog.etEmailSearchMember.text.toString()
+            if (email.isNotEmpty()){
+                dialog.dismiss()
+                // TODO implement adding member logic
+            } else {
+                Toast.makeText(this@MembersActivity, "Please enter members email address.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        dialog.tvCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
