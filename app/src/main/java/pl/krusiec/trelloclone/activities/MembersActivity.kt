11c58b223549ce861b13bf35c3a3e8a48daeac45
Kamil <krusiec.kamil82@gmail.com>
2020-06-28 13:15:21 +0200

    Creating The Member Item And The Toolbar

--- app/src/main/java/pl/krusiec/trelloclone/activities/MembersActivity.kt
+++ app/src/main/java/pl/krusiec/trelloclone/activities/MembersActivity.kt
@@ -2,12 +2,32 @@ package pl.krusiec.trelloclone.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_members.*
 import pl.krusiec.trelloclone.R
+import pl.krusiec.trelloclone.models.Board
+import pl.krusiec.trelloclone.utils.Constants
 
 class MembersActivity : AppCompatActivity() {
+
+    private lateinit var boardDetails: Board
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_members)
+        setupActionBar()
+
+        if (intent.hasExtra(Constants.BOARD_DETAIL)){
+            boardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbarMembersActivity)
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        actionBar?.title = resources.getString(R.string.members)
+
+        toolbarMembersActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
