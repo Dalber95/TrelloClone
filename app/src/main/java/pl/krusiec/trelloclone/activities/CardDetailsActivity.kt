82c9c4540a05502b195af5d85af1501c6dc7ca2f
Kamil <krusiec.kamil82@gmail.com>
2020-06-29 13:25:48 +0200

    Deleting Cards

--- app/src/main/java/pl/krusiec/trelloclone/activities/CardDetailsActivity.kt
+++ app/src/main/java/pl/krusiec/trelloclone/activities/CardDetailsActivity.kt
@@ -1,14 +1,20 @@
 package pl.krusiec.trelloclone.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_card_details.*
 import pl.krusiec.trelloclone.R
+import pl.krusiec.trelloclone.firebase.FirestoreClass
 import pl.krusiec.trelloclone.models.Board
+import pl.krusiec.trelloclone.models.Card
+import pl.krusiec.trelloclone.models.Task
 import pl.krusiec.trelloclone.utils.Constants
 
-class CardDetailsActivity : AppCompatActivity() {
+class CardDetailsActivity : BaseActivity() {
 
     private lateinit var boardDetails: Board
     private var taskListPosition = -1
@@ -23,6 +29,21 @@ class CardDetailsActivity : AppCompatActivity() {
 
         etNameCardDetails.setText(boardDetails.taskList[taskListPosition].cards[cardPosition].name)
         etNameCardDetails.setSelection(etNameCardDetails.text.toString().length)
+
+        btnUpdateCardDetails.setOnClickListener {
+            if (etNameCardDetails.text.toString().isNotEmpty()) {
+                updateCardDetails()
+            } else {
+                Toast.makeText(this@CardDetailsActivity, "Enter card name.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    fun addUpdateTaskListSuccess() {
+        hideProgressDialog()
+        setResult(Activity.RESULT_OK)
+        finish()
     }
 
     private fun setupActionBar() {
@@ -40,6 +61,17 @@ class CardDetailsActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_delete_card ->{
+                alertDialogForDeleteCard(boardDetails.taskList[taskListPosition].cards[cardPosition].name)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun getIntentData() {
         if (intent.hasExtra(Constants.BOARD_DETAIL)) {
             boardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
@@ -50,5 +82,54 @@ class CardDetailsActivity : AppCompatActivity() {
         if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
             cardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
         }
+    }
+
+    private fun updateCardDetails() {
+        val card = Card(
+            etNameCardDetails.text.toString(),
+            boardDetails.taskList[taskListPosition].cards[cardPosition].createdBy,
+            boardDetails.taskList[taskListPosition].cards[cardPosition].assignedTo
+        )
+
+        boardDetails.taskList[taskListPosition].cards[cardPosition] = card
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this@CardDetailsActivity, boardDetails)
+    }
+
+    private fun deleteCard() {
+        val cardsList: ArrayList<Card> = boardDetails.taskList[taskListPosition].cards
+
+        cardsList.removeAt(cardPosition)
+
+        val taskList: ArrayList<Task> = boardDetails.taskList
+        taskList.removeAt(taskList.size - 1)
+
+        taskList[taskListPosition].cards = cardsList
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this@CardDetailsActivity, boardDetails)
+    }
+
+    private fun alertDialogForDeleteCard(cardName: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(resources.getString(R.string.alert))
+        builder.setMessage(
+            resources.getString(
+                R.string.confirmation_message_to_delete_card,
+                cardName
+            )
+        )
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setPositiveButton(resources.getString(R.string.yes)) { dialog, _ ->
+            dialog.dismiss()
+            deleteCard()
+        }
+        builder.setNegativeButton(resources.getString(R.string.no)) { dialog, _ ->
+            dialog.dismiss()
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 }
